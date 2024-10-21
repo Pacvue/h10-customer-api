a9be5ce5385a9b56f3e6f1dd4da44dfcd5e062ad
wenyu <wen.yu@helium10.com>
2024-10-21 16:18:54 +0800

    Initialize Project

--- customer-api/src/main/java/com/pacvue/h10/customer/api/CustomerApplication.java
+++ customer-api/src/main/java/com/pacvue/h10/customer/api/CustomerApplication.java
@@ -3,9 +3,11 @@ package com.pacvue.h10.customer.api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("com.pacvue.h10.customer.api")
+@EnableFeignClients(basePackages = "com.pacvue.h10.customer.api")
 public class CustomerApplication {
     public static void main(String[] args) {
 
